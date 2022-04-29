54af9c9cfe6bdb5c96e3d70c2ad97e2d7b4fe106
LeoUNSA <104027221+LeoUNSA@users.noreply.github.com>
2022-04-29 14:14:07 -0500

    Add files via upload

---
+++ LAB03_GRUPO_C_20210695_Leonardo_Montoya.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+//Ejercicio 1
+/*
+int a,b;
+int Potencia(int a,int b){
+    int c=1,i;
+    for(i=0;i<b;i++)
+        c*=a;
+    return c;
+}
+int main(){
+    cout<<"Ingrese el valor de la base: "<<endl;cin>>a;
+    cout<<"Ingrese el valor del exponente: "<<endl;cin>>b;
+    cout<<"El resultado es: "<<Potencia(a,b)<<endl;
+    return 0;
+}
+*/
+//Ejercicio 2
+/*
+void bisiesto(int a){
+    if (a%4==0){
+        if (a%100==0){
+            if(a%400==0){
+                cout<<"El año es bisiesto."<<endl;
+            }
+            else{
+                cout<<"No es un año bisiesto. "<<endl;
+            }
+        }
+        else{
+            cout<<"El año es bisiesto."<<endl;
+        }
+    }
+    else{
+        cout<<"No es un año bisiesto."<<endl;
+    }
+    return;
+}
+int main(){
+    int a;
+    cout<<"Ingrese el año que le gustaría saber si es bisiesto o no: "<<endl;cin>>a;
+    bisiesto(a);
+    return 0;
+}
+*/
